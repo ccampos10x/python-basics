4657b36e6db0152d0a72b0df49c6d62e77cb0155
Caio <ccampos10x@gmail.com>
2026-01-08 00:55:59 -0300

    Exercicios dados numeros e listas

---
+++ list_ex1_1.py
@@ -0,0 +1,12 @@
+# Peça nomes ao usuário e armazene em uma lista.
+
+#name1 = input("Digite um nome:")
+
+#print (name1)
+# name 1 = 
+#name2 = input()
+
+
+print("início")
+x = input("Digite algo: ")
+print("fim:", x)
