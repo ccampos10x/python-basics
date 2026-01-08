e5aa8b585c0e5810f4ab43aaee5fdd19002a01c1
Caio <ccampos10x@gmail.com>
2026-01-07 23:24:16 -0300

    Dados e numeros update teste

---
+++ dados.py
@@ -0,0 +1,18 @@
+# Nome do usuario 
+name = "Mike"
+
+# Idade do usuario
+age = 23 
+
+# Temperatura corporal do usuario
+bodyTemperature = 40.4 
+
+# Usuario esta logado no sistema
+isLoggedIn = True
+
+
+
+print (name)
+print (age)
+print (bodyTemperature)
+print (isLoggedIn)
